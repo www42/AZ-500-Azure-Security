acb361cc69e32c6f01109e9ffee44b3f8e299f98
www42 <thomas.jaekel@gfn.de>
2020-01-07 16:09:37 +0100

    ps module pim

--- _tj/start.ps1
+++ _tj/start.ps1
@@ -1,9 +1,11 @@
 $PSVersionTable.PSVersion
-Get-Module -ListAvailable -Name Az
+Get-Module -ListAvailable -Name Az*
 
-$ContextMsdn = Get-AzContext -ListAvailable | ? Name -Like "MSDN*"
-Connect-AzAccount -ContextName $ContextMsdn.Name
+# MSDN Context
+#$ContextMsdn = Get-AzContext -ListAvailable | ? Name -Like "MSDN*"
+#Connect-AzAccount -ContextName $ContextMsdn.Name
 
+# AzurePass Context
 $ContextName = "Azure Pass – Sponsorship (84b20f2e-263a-4c9e-b40d-35169bc3b920) - Paul@greta434web.onmicrosoft.com"
 Connect-AzAccount -ContextName $ContextName
 
@@ -16,5 +18,23 @@ Get-AzResourceGroup | Format-Table -Property ResourceGroupName,Location
 # PIM module
 # ----------
 
-# Does not work on MacOS (both 6.2.3 and 7 RC1)
-Find-Module -Name Microsoft.Azure.ActiveDirectory.PIM.PSModule | Install-Module -Force -Verbose
+# Find Module on PowerShell Gallary 
+# old version (oct 2018), only for *Windows* PowerShell (Desk)
+Find-Module -Name Microsoft.Azure.ActiveDirectory.PIM.PSModule | Format-List Name, Version, PublishedDate
+Find-Module -Name Microsoft.Azure.ActiveDirectory.PIM.PSModule | Install-Module -Force -AllowClobber -Verbose
+Get-Command -Module Microsoft.Azure.ActiveDirectory.PIM.PSModule
+
+# Authenticate to PIM as Beate
+Connect-PimService -Username 'Beate@greta434web.onmicrosoft.com'
+
+# Beate is eligible for "Global Administrator" role (Azure AD roles are shown only. No Azure RBAC roles.)
+Get-PrivilegedRoleAssignment
+
+# Activate role
+$params = @{
+    'RoleId' = '62e90394-69f5-4237-9190-012177145e10';
+    'Reason' = 'Testing PowerShell';
+    'Duration' = '0.5'
+}
+
+Enable-PrivilegedRoleAssignment @params
